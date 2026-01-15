54a3516f15d79ec63e5a1c94618c8942535f8493
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-01-15 11:02:07 +0100

    Update utils.ts

--- ppopgi-indexer/src/utils.ts
+++ ppopgi-indexer/src/utils.ts
@@ -1,84 +1,26 @@
-import {
-  ConfigUpdated as ConfigUpdatedEvent,
-  DeployerOwnershipTransferred as DeployerOwnershipTransferredEvent,
-  LotteryDeployed as LotteryDeployedEvent,
-  RegistrationFailed as RegistrationFailedEvent
-} from "../generated/SingleWinnerDeployer/SingleWinnerDeployer"
-import {
-  ConfigUpdated,
-  DeployerOwnershipTransferred,
-  LotteryDeployed,
-  RegistrationFailed
-} from "../generated/schema"
+import { Address, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts"
+import { RaffleEvent, RaffleEventType, Raffle } from "../generated/schema"
 
-export function handleConfigUpdated(event: ConfigUpdatedEvent): void {
-  let entity = new ConfigUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.usdc = event.params.usdc
-  entity.entropy = event.params.entropy
-  entity.provider = event.params.provider
-  entity.feeRecipient = event.params.feeRecipient
-  entity.protocolFeePercent = event.params.protocolFeePercent
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function eventId(event: ethereum.Event): Bytes {
+  return event.transaction.hash.concatI32(event.logIndex.toI32())
 }
 
-export function handleDeployerOwnershipTransferred(
-  event: DeployerOwnershipTransferredEvent
-): void {
-  let entity = new DeployerOwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.oldOwner = event.params.oldOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function touchRaffle(raffle: Raffle, event: ethereum.Event): void {
+  raffle.lastUpdatedBlock = event.block.number
+  raffle.lastUpdatedTimestamp = event.block.timestamp
 }
 
-export function handleLotteryDeployed(event: LotteryDeployedEvent): void {
-  let entity = new LotteryDeployed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.lottery = event.params.lottery
-  entity.creator = event.params.creator
-  entity.winningPot = event.params.winningPot
-  entity.ticketPrice = event.params.ticketPrice
-  entity.name = event.params.name
-  entity.usdc = event.params.usdc
-  entity.entropy = event.params.entropy
-  entity.entropyProvider = event.params.entropyProvider
-  entity.feeRecipient = event.params.feeRecipient
-  entity.protocolFeePercent = event.params.protocolFeePercent
-  entity.deadline = event.params.deadline
-  entity.minTickets = event.params.minTickets
-  entity.maxTickets = event.params.maxTickets
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRegistrationFailed(event: RegistrationFailedEvent): void {
-  let entity = new RegistrationFailed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.lottery = event.params.lottery
-  entity.creator = event.params.creator
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+export function createRaffleEvent(
+  raffleId: Bytes,
+  t: RaffleEventType,
+  event: ethereum.Event
+): RaffleEvent {
+  let e = new RaffleEvent(eventId(event))
+  e.raffle = raffleId
+  e.type = t
+  e.blockNumber = event.block.number
+  e.blockTimestamp = event.block.timestamp
+  e.txHash = event.transaction.hash
+  e.logIndex = BigInt.fromI32(event.logIndex.toI32())
+  return e
 }
